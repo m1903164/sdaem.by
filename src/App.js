517ba7381bd2373565177db32d4d69d95b0da9be
m1903164 <m1903164@edu.misis.ru>
2022-06-07 21:14:48 +0300

    lin-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header className='main-header'>
+        <div className='header__top'>
+           <nav className='top-container'>
+               <ul className='top-list'>
+                  <li className="top-item">
+                     Главная
+                  </li>
+                  <li className="top-item">
+                     Новости
+                  </li>
+                  <li className="top-item">
+                     Размещение и тарифы 
+                  </li>
+                  <li className="top-item">
+                     Объявления на карте
+                  </li>
+                  <li className="top-item">
+                     Контакты
+                  </li>
+               </ul>
+               <div className='top-btn'>
+                  <ul className='top-list__btn'>
+                  <li className="btn-item faivorite">
+                     Закладки
+                  </li>
+                  <li className="btn-item login">
+                     Вход и регистрация
+                  </li>
+                  </ul>
+               </div>
+           </nav>
+        </div>
+        <div className='header__lower'>
+            <nav className='lower-container'>
+               <ul className='lower__list'>
+                  <li className="lower-item">
+                     <img src={logo} className="logo-img" alt="logo" />
+                  </li>
+                  <li className="lower-item">
+                     Квартиры на сутки
+                  </li>
+                  <li className="lower-item">
+                     Коттеджи и усадьбы
+                  </li>
+                  <li className="lower-item">
+                     Бани и Сауны
+                  </li>
+                  <li className="lower-item">
+                     Авто напрокат
+                  </li>
+               </ul>
+               <div className='lower-btn'>
+                  <button className='btn'>
+                     +  Разместить объявление
+                  </button>
+               </div>
+            </nav>
+        </div>
       </header>
     </div>
+    
   );
 }
 
